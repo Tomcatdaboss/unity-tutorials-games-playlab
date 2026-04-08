cfc72f0ba85cb6f5cb2763a1432e9d8042fbda9b
Tomcatdaboss <140665293+Tomcatdaboss@users.noreply.github.com>
2026-04-08 12:09:29 +1000

    Update PlayerMovement2D.cs
    Most recent player movement code!

--- PlayerMovement2D.cs
+++ PlayerMovement2D.cs
@@ -1,54 +1,46 @@
-// NOTE: This script uses the 'new' input system, which has been standard since Unity 6.
-// Any tutorials from before 2025 or so will likely not use this input system.
-// I can show you how to use the old input system in your project - just reach out!
+// NOTE: This script uses the 'old' input system, which has not been standard since Unity 6.
+// Any tutorials from after 2025 or so will likely not use this input system.
 
-// this is where we import packages. Packages are basically bits of code written by other programmers (in this case Unity) that we use to define and use more advanced bits of code, in this case the input system.
+
+// this is where we import packages. Packages are basically bits of code written by other programmers (in this case Unity) that we use to define and use more advanced bits of code.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 public class PlayerMovement2D : MonoBehaviour
 {
     // this section of the script is where the different variables are initialised.
     // To initialise variables in C#, you write <access level> <variable type> <variable name> = <variable value>, as shown below:
-    private CustomInput input = null;
-    private Vector2 moveVector = Vector2.zero;
-    private Rigidbody2D rb = null; // The rigidbody is basically the component that lets the game object it is attached to experience physics, e.g gravity, bouncing off other objects, etc.
+    private Rigidbody2D player = null; // The rigidbody is basically the component that lets the game object it is attached to experience physics, e.g gravity, bouncing off other objects, etc.
     private float moveSpeed = 10f;
-    // The Awake function is called when this script is activated for the first time, e.g upon the start of the game.
-    private void Awake()
-    {  
-        // this code is basically just assigning values to the two variables that we previously left as null.
-        input = new CustomInput();
-        rb = GetComponent<Rigidbody2D>();
+    private float jumpHeight = 5f;
+    private bool is_on_ground = false;
+    // The Start function is called when this script is activated for the first time, e.g upon the start of the game.
+    void Start()
+    { 
+       // this code is basically just assigning values to the variable that we previously left as null.
+       player = GetComponent<Rigidbody2D>();
     }
-    // this OnEnable function is called every time the script is activated in game, not just on start.
-    private void OnEnable()
+    // this Update function is called every frame, making it the main function you will code in.
+    void Update()
+    { // this line of code takes input from the horizontal axis (A and D keys) and uses it to set the player's velocity via the rigidbody component.
+        player.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, player.linearVelocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && is_on_ground)
+        {  // this if statement checks if the player pressed space whilst on the ground and if so, lets them jump.
+            player.linearVelocity = new Vector2(player.linearVelocity.x, jumpHeight);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col) // this method is called whenever the player object contacts another gameobject with a collider.
     {
-        input.Enable();
-        input.Player.Movement.performed += OnMovementPerformed;
-        input.Player.Movement.canceled += OnMovementCanceled;
+        if (col.gameObject.layer == 3) // this if statement checks if the collider you have hit belongs to a gameobject on the 3rd layer (in my tutorial this was our ground layer)
+        { // THE LINE ABOVE MAY NEED TO BE CHANGED DEPENDING ON WHAT LAYER NUMBER YOU HAVE ASSIGNED TO "GROUND"
+            is_on_ground = true;
+        }  
     }
-    // this OnDisable function is called every time the script is de-activated in game
-    private void OnDisable()
+    void OnCollisionExit2D(Collision2D col) // this method is called whenever the player object breaks contact with another gameobject with a collider.
     {
-        input.Disable();
-        input.Player.Movement.performed -= OnMovementPerformed;
-        input.Player.Movement.canceled -= OnMovementCanceled;
-    }
-    // The fixed update function is called many times per second, similar to the standard Update() function that is provided with new scripts, but rather than being called every frame, it's called every 1/50th of a second no matter what, making it useful for physics calculations.
-    private void FixedUpdate()
-    { // every fraction of a second, this line below takes the movement vector (which we got from the input system below) and the move speed (provided by the player) and provides a linear velocity vector to the player rigidbody, making it actually move.
-        rb.linearVelocity = moveSpeed * moveVector;
-    }
-    // this function accepts a new value from the input system and assigns it as the movement direction every time the player moves (or inputs to the specified input system instance, if you want a more general definition)
-    private void OnMovementPerformed(InputAction.CallbackContext value)
-    {
-        moveVector = value.ReadValue<Vector2>();   
-    }
-    // this function sets the player's movement to zero every time the player stops moving/inputting WASD
-    private void OnMovementCanceled(InputAction.CallbackContext value)
-    {
-        moveVector = Vector2.zero;
+        if (col.gameObject.layer == 3) // this if statement checks if the collider you have hit belongs to a gameobject on the 3rd layer (in my tutorial this was our ground layer)
+        {// THE LINE ABOVE MAY NEED TO BE CHANGED DEPENDING ON WHAT LAYER NUMBER YOU HAVE ASSIGNED TO "GROUND"
+            is_on_ground = false;
+        } 
     }
 }
